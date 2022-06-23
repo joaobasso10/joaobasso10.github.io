73e2bf97e7e6e6d933538841875fc86b68ab61bc
joaobasso10 <105473429+joaobasso10@users.noreply.github.com>
2022-06-23 18:45:30 -0300

    script

---
+++ script/script.js
@@ -0,0 +1,27 @@
+// const btn = document.getElementById("icone")
+// const darkmode = document.getElementById("claro")
+
+// btn.addEventListener("click", () =>{
+//     darkmode.classList.toggle("claro")
+//     darkmode.classList.toggle("escuro")   
+// })
+// btn.addEventListener("click", () =>{
+//     btn.classList.toggle("claro")
+//     btn.classList.toggle("escuro")   
+// })
+
+
+const insta = document.getElementById("insta")
+const gmail = document.getElementById("mail")
+const linkedin = document.getElementById("linked")
+
+// https://www.instagram.com/joao_basso10/
+
+insta.addEventListener("click", () => {
+    window.location.href ='https://www.instagram.com/joao_basso10/'
+})
+linkedin.addEventListener("click", () => {
+    window.location.href = 'https://www.linkedin.com/in/jo%C3%A3o-victor-ribeiro-basso-2606b8211/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=br'
+})
+
+
